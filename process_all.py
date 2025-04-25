adfb3ccfc6ce0aa009c33d173d9a844b20527fe6
kaibo <ktang@unc.edu>
2025-04-25 15:34:34 -0400

    Initial commit

---
+++ process_all.py
@@ -0,0 +1,63 @@
+from functools import partial
+import glob
+import os
+
+import numpy as np
+
+from sploss import load_mesh, build_adjacency, find_r_pairs, filter_neighbor_pairs, visualize
+
+
+def process_mesh(mesh_path: str, out_path: str, r: float) -> None:
+    subid = mesh_path.split("/")[-1].split("_")[0]
+    vertices, faces = load_mesh(mesh_path)
+    n_vertices = len(vertices)
+    adj = build_adjacency(faces, n_vertices)
+    pairs = find_r_pairs(vertices, r)
+    pairs = filter_neighbor_pairs(pairs, adj)
+    print(f"subid: {subid}, pairs: {len(pairs)}")
+    out_mesh_path = os.path.join(out_path, f"{os.path.basename(mesh_path).replace('.vtk', '.npy')}")
+    np.save(out_mesh_path, pairs)
+
+
+def visualize_mesh(mesh_path: str, out_path: str, azimuth: float) -> None:
+    subid = mesh_path.split("/")[-1].split("_")[0]
+    vertices, faces = load_mesh(mesh_path)
+    pair_path = os.path.join(out_path, f"{os.path.basename(mesh_path).replace('.vtk', '.npy')}")
+    vtk_path = os.path.join(out_path, f"{os.path.basename(mesh_path)}")
+    png_path = os.path.join(out_path, f"{os.path.basename(mesh_path).replace('.vtk', '.png')}")
+    pairs = np.load(pair_path)
+    print(f"subid: {subid}, pairs: {len(pairs)}")
+    visualize(vertices, faces, pairs, save_vtk_path=vtk_path, save_png_path=png_path, azimuth=azimuth)
+
+
+if __name__ == "__main__":
+    r_lst = [0.4, 0.8, 1.2, 1.6, 2.0]
+    out_path_lst = [f'./r_{str(r).replace(".", "p")}' for r in r_lst]
+    mesh_path_lst = ["./lh.pial.vtk", "./rh.pial.vtk"]
+
+    # process_mesh
+    for r, out_path in zip(r_lst, out_path_lst):
+        if not os.path.exists(out_path):
+            os.makedirs(out_path)
+        process_mesh_partial = partial(process_mesh, out_path=out_path, r=r)
+        for mesh_path in mesh_path_lst:
+            process_mesh_partial(mesh_path=mesh_path, out_path=out_path, r=r)
+    
+    # visualize_mesh
+    for r, out_path in zip(r_lst, out_path_lst):
+        visualize_mesh_partial = partial(visualize_mesh, out_path=out_path)
+        visualize_mesh_partial(mesh_path_lst[0], azimuth=180.0)
+        visualize_mesh_partial(mesh_path_lst[1], azimuth=0.0)
+
+    import matplotlib.pyplot as plt
+    f, ax = plt.subplots(5, 2, figsize=(8, 16), layout='constrained')
+    for i, r in enumerate(r_lst):
+        for j, mesh_path in enumerate(mesh_path_lst):
+            title = "Left hemisphere" if j == 0 else "Right hemisphere"
+            png_path = os.path.join(out_path_lst[i], f"{os.path.basename(mesh_path).replace('.vtk', '.png')}")
+            img = plt.imread(png_path)
+            ax[i, j].imshow(img)
+            ax[i, j].axis('off')
+            ax[i, j].set_title(f"{title}, r={r}")
+    f.savefig("surf.png", dpi=600)
+    plt.show()
---
+++ sploss.py
@@ -0,0 +1,100 @@
+from typing import Tuple
+
+import numpy as np
+import pyvista as pv
+import scipy.sparse as sp
+from scipy.spatial import KDTree
+import torch
+import torch.nn as nn
+
+
+def load_mesh(mesh_path: str) -> Tuple[np.ndarray, np.ndarray]:
+    mesh = pv.read(mesh_path)
+    return mesh.points.astype(np.float32), mesh.faces.reshape(-1, 4)[:, 1:].astype(np.int64)
+
+
+def find_r_pairs(vertices: np.ndarray, r: float) -> np.ndarray:
+    return KDTree(vertices).query_pairs(r, output_type='ndarray')
+
+
+def filter_neighbor_pairs(pairs: np.ndarray, adj: np.ndarray) -> np.ndarray:
+    return np.asarray([(i, j) for i, j in pairs if not np.any(adj[i] & adj[j])], dtype=np.int64)
+
+
+def build_adjacency(faces: np.ndarray, n_vertices: int) -> np.ndarray:
+    edge_pairs = np.vstack([faces[:, [0, 1]], faces[:, [1, 2]], faces[:, [2, 0]]])
+    edges_undirected = np.vstack([edge_pairs, edge_pairs[:, ::-1]])
+    row, col = edges_undirected.T
+    data = np.ones(len(row), dtype=bool)
+    adj = sp.coo_matrix((data, (row, col)), shape=(n_vertices, n_vertices))
+    return adj.toarray()
+
+
+def visualize(
+    vertices: np.ndarray, faces: np.ndarray, pairs: np.ndarray, n_show: int | None = None, 
+    save_vtk_path: str | None = None, save_png_path: str | None = None, azimuth: float = 0.0,
+) -> None | pv.pyvista_ndarray:
+    n_show = len(pairs) if n_show is None else n_show
+
+    # surface
+    tri_flag = np.full((faces.shape[0], 1), 3, dtype=np.int64)
+    poly = pv.PolyData(vertices, np.hstack([tri_flag, faces]))
+
+    # lines
+    rng = np.random.default_rng(seed=0)
+    sel = rng.choice(len(pairs), size=min(n_show, len(pairs)), replace=False)
+    pts_l = vertices[pairs[sel, 0]]
+    pts_r = vertices[pairs[sel, 1]]
+    n = pts_l.shape[0]
+    pts = np.zeros((2 * n, 3))
+    pts[0::2] = pts_l
+    pts[1::2] = pts_r
+    n_lines = pts.shape[0] // 2
+    connectivity = np.hstack([
+        [2, 2 * i, 2 * i + 1] for i in range(n_lines)
+    ]).astype(np.int64)
+    lines_mesh = pv.PolyData(pts, lines=connectivity)
+    if save_vtk_path is not None:
+        lines_mesh.save(save_vtk_path)
+
+    # plot
+    if save_png_path is None:
+        pl = pv.Plotter()
+        pl.add_mesh(poly, opacity=1.0, color="lightslategray")
+        pl.add_mesh(lines_mesh, color='red', line_width=1)
+        pl.camera_position = 'yz'
+        pl.camera.azimuth = azimuth
+        pl.camera.zoom(1.5)
+        pl.show()
+    else:
+        pl = pv.Plotter(off_screen=True)
+        pl.add_mesh(poly, opacity=1.0, color="lightslategray")
+        pl.add_mesh(lines_mesh, color='red', line_width=1)
+        pl.camera_position = 'yz'
+        pl.camera.azimuth = azimuth
+        pl.camera.zoom(1.5)
+        img = pl.screenshot(save_png_path, window_size=[3840, 2160])
+        pl.close()
+        return img
+
+
+class SelfProximityLoss(nn.Module):
+    def __init__(self, delta: float, reduction: str = 'mean'):
+        super().__init__()
+        self.delta = delta
+        if reduction not in ['none', 'sum', 'mean']:
+            raise ValueError(f"Invalid reduction mode: {reduction}. Expected one of 'none', 'mean', 'sum'.")
+        self.reduction = reduction
+    def forward(self, vertices: torch.Tensor, pairs: np.ndarray) -> torch.Tensor:
+        v0 = vertices[:, pairs[:, 0], :]
+        v1 = vertices[:, pairs[:, 1], :]
+        d = torch.linalg.norm(v0 - v1, dim=-1)
+        p = torch.zeros_like(d)
+        p[d < self.delta] = (self.delta - d[d < self.delta]) ** 2
+
+        if self.reduction == 'none':
+            return p
+        elif self.reduction == 'sum':
+            return p.sum()
+        else:
+            return p.sum() / torch.gt(p, 0).sum()
